7e996656237f71a31546cec16b6079bd7f23b940
slavanorm <512f@bk.ru>
2025-04-22 22:28:31 +0400

    next

--- nvim/lua/autocommand.lua
+++ nvim/lua/autocommand.lua
@@ -88,8 +88,8 @@ vim.api.nvim_create_autocmd('BufEnter', {
 vim.api.nvim_create_autocmd('VimEnter', {
     callback = function()
         -- load plugin only when invoked with path
-        if #vim.v.argv > 2 and vim.fn.isdirectory(vim.v.argv[3]) then
-            require 'persistence'.load()
+        if #vim.v.argv > 2 and vim.fn.getftype(vim.v.argv[3]) == 'dir' then
+                require 'persistence'.load()
         end
     end
 })
