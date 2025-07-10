5814359d763208e369707f7656da1f01cd47443a
slavanorm <512f@bk.ru>
2025-07-10 18:44:43 +0300

    next

--- nvim/lua/plugins/tbd.lua
+++ nvim/lua/plugins/tbd.lua
@@ -1,17 +1,26 @@
 return {
-    "benlubas/wrapping-paper.nvim", 
+    "benlubas/wrapping-paper.nvim",
     -- manage text wrap. does it need any customize? i like how it is now.
-    "polarmutex/git-worktree.nvim", 
+    "polarmutex/git-worktree.nvim",
     -- git worktree integration. idk how does it work
-    "tpope/vim-fugitive", 
+    "tpope/vim-fugitive",
     -- alternative to git signs, i show branch in line with it
-    "mbbill/undotree", 
+    "mbbill/undotree",
     -- show undos as tree. idk how to toggle it etc
-    { 'williamboman/mason.nvim',   enabled = false },
-    { 'catppuccin/nvim',   enabled = false },
-    { 'williamboman/mason-lspconfig.nvim',   enabled = false },
-    { 'mason-org/mason-lspconfig.nvim',   enabled = false },
-    { 'nvim-lualine/lualine.nvim', enabled = false },
-    { 'akinsho/bufferline.nvim',   enabled = false },
+    { 'williamboman/mason.nvim',           enabled = false },
+    { 'catppuccin/nvim',                   enabled = false },
+    { 'williamboman/mason-lspconfig.nvim', enabled = false },
+    { 'mason-org/mason-lspconfig.nvim',    enabled = false },
+    { 'nvim-lualine/lualine.nvim',         enabled = false },
+    { 'akinsho/bufferline.nvim',           enabled = false },
     --{ 'saghen/blink.cmp',          enabled = false }
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim", -- required
+            "sindrets/diffview.nvim", -- optional - Diff integration
+            "nvim-telescope/telescope.nvim", -- optional
+            "folke/snacks.nvim",     -- optional
+        },
+    }
 }
